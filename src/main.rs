22ab165800262a73df53a5cdbfcf2168904417d7
Matt Windsor <mattwindsor@btinternet.com>
2014-11-25 21:28:35 +0000

    Aftermath from various attempts to close stdin.
    This doesn't seem to be something that can be done in Rust yet.  Aw.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![feature(if_let)]
 
 extern crate baps3_protocol;
+extern crate libc;
+
 use baps3_protocol::Unpacker;
 use client::{Client, Request, Response};
+
+use libc::{close, STDIN_FILENO};
 
 mod client;
 
@@ -10,6 +14,19 @@ fn main() {
     match Client::new("127.0.0.1:1350") {
         Ok(client) => client_main_loop(client),
         Err(e) => println!("{}", e)
+    }
+}
+
+fn send_message(
+    request_tx: &Sender<Request>,
+    unpacker: &mut Unpacker,
+    message: &str
+) {
+    for pline in unpacker.feed(message.as_slice()).iter() {
+        if let [ref cmd, args..] = pline.as_slice() {
+            println!("> {} {}", cmd, args);
+            request_tx.send(Request::SendMessage(cmd.clone(), args.to_vec()));
+        }
     }
 }
 
@@ -22,20 +39,26 @@ fn client_main_loop(Client {
 
         for line in std::io::stdin().lines() {
             match line {
-                Ok(l) => for pline in u.feed(l.as_slice()).iter() {
-                    if let [ref cmd, args..] = pline.as_slice() {
-                        println!("> {} {}", cmd, args);
-                        request_tx.send(Request::SendMessage(cmd.clone(),
-                                                             args.to_vec()));
-                    }
-                },
-                Err(e) => println!("{}", e)
+                Ok(l) => send_message(&request_tx, &mut u, l.as_slice()),
+                Err(e) => {
+                    println!("{}", e)
+                    return;
+                }
             }
         }
 
         request_tx.send(Request::Quit);
     });
 
+    response_iter(response_rx);
+
+    println!("Closing...");
+    unsafe {
+        close(STDIN_FILENO);
+    }
+}
+
+fn response_iter(response_rx: Receiver<Response>) {
     for m in response_rx.iter() {
         match m {
             Response::Message(cmd, args) => println!("< {} {}", cmd, args),
