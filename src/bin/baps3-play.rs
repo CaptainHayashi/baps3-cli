73dd6aa50dfb70dcd4afba20353b8e4ff4a94895
Matt Windsor <mattwindsor@btinternet.com>
2014-12-07 03:25:38 +0000

    Add play and stop commands.
    Because why not?

---
+++ src/bin/baps3-play.rs
@@ -0,0 +1,38 @@
+#![feature(phase)]
+
+extern crate baps3_protocol;
+extern crate baps3_cli;
+#[phase(plugin)] extern crate baps3_cli;
+extern crate docopt;
+extern crate serialize;
+#[phase(plugin)] extern crate docopt_macros;
+
+use baps3_cli::{one_shot, verbose_logger};
+use baps3_cli::client::Client;
+
+docopt!(Args deriving Show, "
+Plays the currently loaded file in a BAPS3 server.
+
+Usage:
+  baps3-play [options]
+
+Options:
+  -h, --help             Show this message.
+  -t, --target <target>  The target BAPS3 server (host:port).
+                         [Default: localhost:1350]
+  -v, --verbose          Prints a trail of miscellaneous information
+                         about the action.
+")
+
+fn main() {
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    let mut log = verbose_logger(args.flag_verbose);
+
+    Client::new(&*args.flag_target)
+      .and_then(|c| one_shot(&mut log,
+                             c,
+                             &["PlayStop"],
+                             "play",
+                             &[]))
+      .unwrap_or_else(|e| werr!("error: {}", e));
+}
